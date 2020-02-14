4a40b09903e0a409f2d18d6b417d1cf69abedbfb
oliverlowbob <oliverlowbob@live.dk>
2020-02-14 08:42:49 +0100

    Initial Commit

---
+++ week07/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  week07
+//
+//  Created by admin on 14/02/2020.
+//  Copyright © 2020 admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
